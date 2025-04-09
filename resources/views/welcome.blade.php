18e855448fb3833bfaecc37d027f98e6f4bf7c63
Joan <joanlima2005@gmail.com>
2025-04-09 13:55:20 +0200

    fix: Update sidebar repository link and improve welcome page layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,55 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
-                    @auth   
-                        <a
-                            href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Dashboard
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Log in
-                        </a>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <title>Laravel</title>
 
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                                Register
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
-        </header>
-        <div class="Card w-full max-w-[700px] bg-[#adbaba] dark:bg-[#0a0a0a] rounded-lg shadow-lg p-10">
-            <div class="Card-header flex gap-2 flex-col justify-between">
-                <p class="text-3xl text-center">Welcome to <b>Joan Blog</b></p>
-                <p class="text-sm font-bold text-center"> ! Have fun and enjoy !</p>
-            </div>
-            <div class="Card-body flex gap-4 flex-col justify-between mt-7">
-                <p class="text-sm text-center">Welcome to the blog. A recreational space to post news, previews, or interesting facts about the topics covered. Register to participate in this experience or log in to continue publishing.</p>
-            </div>
-        </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+</head>
+
+<body
+    class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-5 not-has-[nav]:hidden">
         @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
+            <nav class="flex items-center justify-end gap-4">
+                @auth
+                    <a href="{{ url('/dashboard') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                        Dashboard
+                    </a>
+                @else
+                    <a href="{{ route('login') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
+                        Log in
+                    </a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                            Register
+                        </a>
+                    @endif
+                @endauth
+            </nav>
         @endif
-    </body>
+    </header>
+    <div class="Card w-full max-w-[700px] bg-[#adbaba] dark:bg-[#0a0a0a] rounded-lg shadow-lg p-10 mt-10">
+        <div class="Card-header flex gap-4 flex-col justify-between">
+            <p class="text-3xl text-center">Welcome to <b>Joan Blog</b></p>
+            <p class="text-sm font-bold text-center"> ! Have fun and enjoy !</p>
+        </div>
+        <div class="Card-body flex gap-4 flex-col justify-between mt-5">
+            <p class="text-sm text-center">Welcome to the blog. A recreational space to post news, previews, or
+                interesting facts about the topics covered. Register to participate in this experience or log in to
+                continue publishing.</p>
+        </div>
+    </div>
+</body>
+
 </html>
